7044443081886b1bdf1bfe45310c01f1b4a9dfee
Rupert <rbroad@ohpen.com>
2021-05-12 12:50:58 +0200

    lanz-943 update scripts

--- scripts/tfm_init.sh
+++ scripts/tfm_init.sh
@@ -2,17 +2,26 @@
 chmod +x scripts/*.sh
 WKDIR=$PWD
 
-cd ./modules || terraform init -input=false -backend=false; if [ $? != "0" ]; then exit $?; fi
-for f in *; do
-  if [ -d "$f" ]; then
-    # cycle through each module dir and initialize
-    cd "$f" || exit
-    echo -e "\n## Init module $f"
-    if ! terraform init -input=false -backend=false; then exit $?; fi
-    source "$WKDIR"/scripts/lint.sh
-    source "$WKDIR"/scripts/validate.sh
-    cd ..
-  fi
-done
-cd ..
-            
+run_scripts(){
+  if ! terraform init -input=false -backend=false; then exit $?; fi
+  source "$WKDIR"/scripts/lint.sh
+  source "$WKDIR"/scripts/validate.sh
+  source "$WKDIR"/scripts/tfm_docs.sh
+}
+
+if [ -d modules ]; then
+  cd ./modules || exit
+  for f in *; do
+    if [ -d "$f" ]; then
+      # cycle through each module dir and initialize
+      cd "$f" || exit
+      echo -e "\n## Init module $f"
+      run_scripts
+      cd ..
+    fi
+  done
+  cd ..
+else
+  run_scripts
+fi   
+          
--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "name" {
-  description = "Name to apply to resources"
-}
-variable "region" { default = "eu-west-1" }
-variable "tags" {
-  type        = map(any)
-  description = "(Required) Map of tags to apply to repository"
-}
