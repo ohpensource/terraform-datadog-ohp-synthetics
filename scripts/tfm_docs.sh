9d3d1daca1e6dfab7452a9197287403d56913af3
Rupert <rbroad@ohpen.com>
2021-05-12 20:40:39 +0200

    tfm-docs

--- scripts/tfm_docs.sh
+++ scripts/tfm_docs.sh
@@ -5,6 +5,7 @@ TFM_DOCS_VER="0.13.0"
 MOD_DIR="modules"
 
 if ! command -v terraform-docs >/dev/null 2>&1; then
+  echo -e "\n ## Installing terraform-docs"
   curl -Lo ./terraform-docs.tar.gz "https://github.com/terraform-docs/terraform-docs/releases/download/v${TFM_DOCS_VER}/terraform-docs-v${TFM_DOCS_VER}-linux-amd64.tar.gz"
   tar -xzf terraform-docs.tar.gz 
   chmod +x terraform-docs 
@@ -13,15 +14,13 @@ fi
 
 # Create docs for each module
 if [ -d "$MOD_DIR" ]; then
-  for f in $(ls $MOD_DIR); do
-    if [ -d "./$MOD_DIR/$f" ]; then
+  for f in $(ls -d ./$MOD_DIR/*); do
       # cycle through each module dir and create docs
-      echo -e "\n## Creating terraform docs for module $f"
-      terraform-docs markdown table --sort-by required --output-file README.md "./$MOD_DIR/$f" 
+      echo -e "\n ## Creating terraform docs for module $f"
+      terraform-docs markdown table --sort-by required --output-file "./$MOD_DIR/$f/README.md" "./$MOD_DIR/$f" 
       git add "./$MOD_DIR/$f/README.md"
-    fi
   done
 else
-  echo -e "\n## Creating terraform docs for root module"
+  echo -e "\n ## Creating terraform docs for root module"
   terraform-docs markdown table --sort-by required --output-file  README.md .
 fi  
