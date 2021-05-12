c4c64e403139d1de59160c9aec157782ce4e5478
Rupert <rbroad@ohpen.com>
2021-05-12 13:04:20 +0200

    ensure install happens only once

--- scripts/tfm_docs.sh
+++ scripts/tfm_docs.sh
@@ -3,9 +3,11 @@
 # Install Terraform-docs
 TFM_DOCS_VER="0.13.0"
 
-curl -Lo ./terraform-docs.tar.gz "https://github.com/terraform-docs/terraform-docs/releases/download/v${TFM_DOCS_VER}/terraform-docs-v${TFM_DOCS_VER}-linux-amd64.tar.gz"
-tar -xzf terraform-docs.tar.gz 
-chmod +x terraform-docs 
-mv terraform-docs /usr/local/bin/terraform-docs
+if ! command -v terraform-docs >/dev/null 2>&1; then
+  curl -Lo ./terraform-docs.tar.gz "https://github.com/terraform-docs/terraform-docs/releases/download/v${TFM_DOCS_VER}/terraform-docs-v${TFM_DOCS_VER}-linux-amd64.tar.gz"
+  tar -xzf terraform-docs.tar.gz 
+  chmod +x terraform-docs 
+  mv terraform-docs /usr/local/bin/terraform-docs
+fi
 
-/usr/local/bin/terraform-docs --sort-by-required markdown table ./ > README.md
+terraform-docs markdown table --output-file  README.md . --sort-by required
