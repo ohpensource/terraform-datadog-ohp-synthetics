23181a0ec8c2bcada030ed25f59fc8923c53e135
Rupert <rbroad@ohpen.com>
2021-05-12 12:57:31 +0200

    install tfm docs

--- scripts/tfm_docs.sh
+++ scripts/tfm_docs.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Install Terraform-docs
+TFM_DOCS_VER="0.13.0"
+
+curl -Lo ./terraform-docs.tar.gz "https://github.com/terraform-docs/terraform-docs/releases/download/v${TFM_DOCS_VER}/terraform-docs-v${TFM_DOCS_VER}-linux-amd64.tar.gz"
+tar -xzf terraform-docs.tar.gz 
+chmod +x terraform-docs 
+mv terraform-docs /usr/local/bin/terraform-docs
+
 terraform-docs --sort-by-required markdown table ./ > README.md
