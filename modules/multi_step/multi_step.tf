2b2f057ea115f8f0a686db8a983e1a8fe775686f
mpivarnikohpen <91672258+mpivarnikohpen@users.noreply.github.com>
2022-01-19 20:31:32 +0100

    Update multi_step.tf

--- modules/multi_step/multi_step.tf
+++ modules/multi_step/multi_step.tf
@@ -87,6 +87,11 @@ resource "datadog_synthetics_test" "multi" {
           }
         }
       }
+      
+      retry {
+        count    = var.options_list.retry_count
+        interval = var.options_list.retry_interval
+    }
     }
   }
 
